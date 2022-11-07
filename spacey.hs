a1baf866c7a68e3f5d998bc143bd3f26eec7c084
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-11-07 17:32:38 +0100

    feat:add test whitespace source files

--- spacey.hs
+++ spacey.hs
@@ -1,8 +1,16 @@
 import System.Environment
+import Control.Applicative (Alternative (..))
+import Data.List (nub)
+
+data ParseError i e = EndOfFile | Unexpected i | CustomError e | Empty deriving (Eq, Show)
+newtype Parser i e a = Parser { runParser :: [i] -> Either [ParseError i e] (a, [i]) }
+
+ast :: String -> String
+ast = return "test"
 
 main :: IO ()
 main = do
         (file:_) <- getArgs
         source <- readFile file
-        putStrLn source
+        putStrLn (ast source)
         return ()
